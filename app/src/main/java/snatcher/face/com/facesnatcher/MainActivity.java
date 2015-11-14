f98c3211e50608b12b4e2633ec3fdf05c05b8b7a
TakuKobayashi <keep_slimbody@yahoo.co.jp>
2015-11-14 14:14:08 +0900

    基本的な部分をとりあえず以降してきました

--- app/src/main/java/snatcher/face/com/facesnatcher/MainActivity.java
+++ app/src/main/java/snatcher/face/com/facesnatcher/MainActivity.java
@@ -1,52 +1,54 @@
 package snatcher.face.com.facesnatcher;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.ArrayList;
+
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
+public class MainActivity extends Activity {
+    private static int REQUEST_CODE = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+        ButterKnife.bind(this);
+        requestPermission();
+    }
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+    private void requestPermission(){
+        if(Build.VERSION.SDK_INT >= 23) {
+            ArrayList<String> permissions = ApplicationHelper.getSettingPermissions(this);
+            boolean isRequestPermission = false;
+            for(String permission : permissions){
+                if(!ApplicationHelper.hasSelfPermission(this, permission)){
+                    isRequestPermission = true;
+                    break;
+                }
             }
-        });
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+            if(isRequestPermission) {
+                requestPermissions(permissions.toArray(new String[0]), REQUEST_CODE);
+            }
         }
+    }
 
-        return super.onOptionsItemSelected(item);
+    @OnClick(R.id.button)
+    public void onClickButton(View v) {
+//        Intent intent = new Intent(this, CameraActivity.class);
+//        startActivity(intent);
+    }
+
+    @OnClick(R.id.button2)
+    public void onClickButton2(View v) {
+//        Intent intent = new Intent(this, SoundActivity.class);
+//        startActivity(intent);
     }
 }
